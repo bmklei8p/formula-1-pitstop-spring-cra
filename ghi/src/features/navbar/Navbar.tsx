01a34b72a4184e5f1d2cb7cd8ff9d3310c3a2604
Bryan Kleinberg <bmkleip@gmail.com>
2023-03-24 22:30:43 -0400

    updated logic on navbar button to toggle light/darkmode

--- ghi/src/features/navbar/Navbar.tsx
+++ ghi/src/features/navbar/Navbar.tsx
@@ -1,13 +1,22 @@
 import { useTheme } from "@mui/material/styles"
-import { useMediaQuery } from '@mui/material'
+import { Button, useMediaQuery } from '@mui/material'
+import { useAppSelector } from "../../app/hooks";
+import { useState } from "react";
+import { useAppDispatch } from "../../app/hooks";
+import { toggle } from "../ModeSlice";
 
 export const Navbar = () => {
   const theme = useTheme();
   // matches is true when media is a phone/tablet size
   const matches = useMediaQuery(theme.breakpoints.down('md'));
+  const mode = useAppSelector((state) => state.mode.darkMode);
+  const dispatch = useAppDispatch();
 
   return (
     <>
+      <Button onClick={() => dispatch(toggle((mode === "dark") ? "light" : "dark"))}>
+        Toggle Dark/Light
+      </Button>
       {matches ? <h1>Mobile</h1> : <h2>Desktop</h2>}
     </>
   )
